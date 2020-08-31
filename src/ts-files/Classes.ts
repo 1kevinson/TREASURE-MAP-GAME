39212633047116deacc89176fb7d2e67c8bc549f
Arsene Kevin <kevinkouomeu@icloud.com>
2020-08-31 13:48:51 +0200

    Add model for map entities

--- src/ts-files/Classes.ts
+++ src/ts-files/Classes.ts
@@ -1,8 +1,80 @@
-class Grid {
+export enum Orientation {
+  NORTH = "N",
+  SOUTH = "S",
+  WEST = "W",
+  EAST = "E",
+}
+
+// Position on the multidimensional array Grid
+export type GridPosition = {
+  x: number;
+  y: number;
+};
+
+// The moves of the adventurer
+export enum Direction {
+  LEFT = "G",
+  RIGHT = "D",
+}
+
+abstract class Grid {
   value: string;
-  constructor(value: string) {
+  protected constructor(value: string) {
     this.value = value;
   }
 }
 
-export { Grid };
+class EmptyGrid extends Grid {
+  constructor(value: string) {
+    super(value);
+  }
+}
+
+class Mountain extends Grid {
+  constructor(value: string) {
+    super(value);
+  }
+}
+
+class Treasure extends Grid {
+  quantity: number;
+
+  constructor(value: string, quantity: number) {
+    super(value);
+    this.quantity = quantity;
+  }
+
+  decrease(): void {
+    this.quantity--;
+  }
+
+  getValue(): string {
+    return `T(${this.quantity})`;
+  }
+}
+
+class Adventurer extends Grid {
+  name: string;
+  treasureCollected: number;
+  position: GridPosition;
+  orientation: Orientation;
+
+  constructor(
+    value: string,
+    name: string,
+    position: GridPosition,
+    orientation: Orientation
+  ) {
+    super(value);
+    this.name = name;
+    this.treasureCollected = 0;
+    this.position = position;
+    this.orientation = orientation;
+  }
+
+  addTreasure(): void {
+    this.treasureCollected++;
+  }
+}
+
+export { Grid, EmptyGrid, Mountain, Treasure, Adventurer };
