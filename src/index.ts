3af612b7f1f9cf388b1eae619f00d44d07f97e94
Arsene Kevin <kevinkouomeu@icloud.com>
2020-08-31 14:47:05 +0200

    add card file

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { Direction, EmptyGrid, Adventurer, Treasure } from "./ts-files/Classes";
+
+//Get DOM Elements
+const inputFile = document.querySelector("#file-input");
+const btnStart = document.querySelector("#btn-start");
+const btnResult = document.querySelector("#btn-result");
