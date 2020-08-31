871009864e7c3dfd543735f6202e47a92284dc2a
Arsene Kevin <kevinkouomeu@icloud.com>
2020-08-31 16:34:59 +0200

    Add Validator.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
+// @ts-nocheck
 import { Direction, EmptyGrid, Adventurer, Treasure } from "./ts-files/Classes";
+import { validateFile } from "./ts-files/Validator";
 
 //Get DOM Elements
 const inputFile = document.querySelector("#file-input");
 const btnStart = document.querySelector("#btn-start");
 const btnResult = document.querySelector("#btn-result");
+
+//Disable buttons
+btnStart.style.disabled;
 
 //Get file entered by user
 inputFile.addEventListener("change", (e) => {
@@ -22,3 +27,17 @@ inputFile.addEventListener("change", (e) => {
 
   reader.readAsText(e.target.files[0]);
 });
+
+btnStart.addEventListener("click", (e) => {
+  console.log(content);
+  console.log(validateFile(content));
+});
+
+const content = `C​ - 3 - 4
+M​ - 1 - 0
+M​ - 2 - 1
+T​ - 0 - 3 - 2
+T​ - 1 - 3 - 3
+A​ - Lara - 1 - 1 - S - AADADAGGA`;
+
+const isValid = validateFile(content);
---
+++ src/ts-files/Validator.ts
@@ -0,0 +1,67 @@
+const isMapOrMountainValid = (line: string, firstLineChar: string): boolean => {
+  if (line.indexOf(firstLineChar) !== 0) return false;
+
+  const array = line.split("-");
+  if (array.length !== 3) return false;
+
+  return !(isNaN(Number(array[1])) || isNaN(Number(array[2])));
+};
+
+const isTreasureValid = (line: string): boolean => {
+  if (line.indexOf("T") !== 0) return false;
+
+  const array = line.split("-");
+  if (array.length !== 4) return false;
+
+  return !(
+    isNaN(Number(array[1])) ||
+    isNaN(Number(array[2])) ||
+    isNaN(Number(array[3]))
+  );
+};
+
+const isAdventurerValid = (line: string): boolean => {
+  if (line.indexOf("A") !== 0) return false;
+
+  const array = line.split("-");
+  if (array.length !== 6) return false;
+
+  if (isNaN(Number(array[2])) || isNaN(Number(array[3]))) return false;
+
+  if (["N", "S", "W", "E"].indexOf(array[4].trim()) < 0) return false;
+
+  return /^[ADG]+$/.test(array[5].trim());
+};
+
+const validateFile = (content: string): boolean => {
+  const lines = content.split("\n");
+
+  let isValid = isMapOrMountainValid(lines[0], "C");
+
+  for (let i = 1; i < lines.length; i++) {
+    //End Loop if the file doesn't start with "C" for Card
+    if (!isValid) {
+      break;
+    }
+    //Ignore the comments in file
+    if (lines[i].indexOf("#") === 0) {
+      continue;
+    }
+
+    if (lines[i].indexOf("M") === 0) {
+      isValid = isValid && isMapOrMountainValid(lines[i], "M");
+    }
+
+    if (lines[i].indexOf("T") === 0) {
+      isValid = isValid && isTreasureValid(lines[i]);
+    }
+
+    if (lines[i].indexOf("A") === 0) {
+      isValid = isAdventurerValid(lines[i]);
+    }
+  }
+
+  return isValid;
+};
+
+export { validateFile };
