14a155e9081399e5fd78cf58eb6f5917bb06e56b
Arsene Kevin <kevinkouomeu@icloud.com>
2020-08-31 15:29:53 +0200

    Load the file

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,21 @@ import { Direction, EmptyGrid, Adventurer, Treasure } from "./ts-files/Classes";
 const inputFile = document.querySelector("#file-input");
 const btnStart = document.querySelector("#btn-start");
 const btnResult = document.querySelector("#btn-result");
+
+//Get file entered by user
+inputFile.addEventListener("change", (e) => {
+  const reader = new FileReader();
+  const filename = e.target.value.split("\\")[2];
+
+  //Change the label onChange of input file
+  e.target.nextElementSibling.innerText = "../" + filename;
+
+  reader.onload = function (event) {
+    const contents = event.target.result;
+  };
+  reader.onerror = function (event) {
+    console.error("File could not be read! Code " + event.target.error.code);
+  };
+
+  reader.readAsText(e.target.files[0]);
+});
